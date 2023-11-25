dcaa54c460ec7f30a31894f5f71b0f00015455a4
Paul <paul.mshtk.707@gmail.com>
2023-11-25 13:09:32 +0200

    added test template for testcase [#01]

---
+++ tests/RegisterUser.spec.js
@@ -0,0 +1,56 @@
+const { test, expect } = require("@playwright/test");
+const { HomePage } = require("../pages/home.page");
+const { SignupLoginPage } = require("../pages/signuplogin.page");
+const { SignupPage } = require("../pages/signup.page");
+const { AccountCreationPage } = require("../pages/account_created.page");
+const { AccountDeletionPage } = require("../pages/account_deleted.page");
+
+test.describe("User Registration and Account Deletion", () => {
+  let page;
+
+  test.beforeAll(async ({ browser }) => {
+    page = await browser.newPage();
+  });
+
+  test.afterAll(async () => {
+    await page.close();
+  });
+
+  test("User should be able to register, log in, delete the account, and observe appropriate messages", async () => {
+    const homePage = new HomePage(page);
+    const signupLoginPage = new SignupLoginPage(page);
+    const signupPage = new SignupPage(page);
+    const accountCreationPage = new AccountCreationPage(page);
+    const accountDeletionPage = new AccountDeletionPage(page);
+
+    await homePage.openUrl(process.env.URL);
+    // Add assertions to verify home page visibility
+
+    await homePage.clickHeaderSighnupLoginBttn();
+    // Add assertions to verify 'New User Signup!' section visibility
+
+    await signupLoginPage.enterNameAndEmail();
+    // Add assertions to verify name and email entry
+
+    await signupLoginPage.clickSignupButton();
+    // Add assertions to verify 'ENTER ACCOUNT INFORMATION' section visibility
+
+    await signupPage.fillAccountInformation();
+    // Add assertions for successful account details submission
+
+    await signupPage.fillAddressInformation();
+    // Add assertions for successful account details submission
+
+    await signupPage.clickCreateAccountButton();
+    // Add assertions to verify 'ACCOUNT CREATED!' message visibility
+
+    await accountCreationPage.clickContinueBttn();
+    // Add assertions to verify successful login message
+
+    await homePage.clickHeaderDeleteAccBttn();
+    // Add assertions to verify 'ACCOUNT DELETED!' message visibility
+
+    await accountDeletionPage.clickContinueBttn();
+    // Add assertions to verify successful deletion and navigation after account deletion
+  });
+});

787ae7cdb475949931cf5c57aa2fa8425ed2c26c
Paul <paul.mshtk.707@gmail.com>
2023-12-11 09:17:14 +0200

    refactor acc creation/deletion pages

--- pages/account_created.page.js
+++ pages/account_created.page.js
@@ -7,21 +7,13 @@ class AccountCreationPage extends Page {
     constructor(page) {
         super(page)
         this.page = page
-        this.accCreatedMssg = accCreatedMssg;
-        this.continueBttn = continueBttn;
     }
 
-    // async getAccCreatedMssg() {
-    //     return await super.getElement(accCreatedMssg)
-    // }
-    // async getContinueBttn() {
-    //     return await super.getElement(continueBttn)
-    // }
     async clickContinueBttn() {
         return await super.clickElement(continueBttn)
     }
     async isAccountCreatedVisible() {
-        const isVisible = await this.page.isVisible(this.accCreatedMssg);
+        const isVisible = await this.page.isVisible(accCreatedMssg);
         return isVisible;
     }
 }
--- pages/account_deleted.page.js
+++ pages/account_deleted.page.js
@@ -7,20 +7,13 @@ class AccountDeletionPage extends Page {
     constructor(page) {
         super(page)
         this.page = page
-        this.accDeletedMssg = accDeletedMssg;
     }
 
-    // async getAccDeletedMssg() {
-    //     return await super.getElement(accDeletedMssg)
-    // }
-    // async getContinueBttn() {
-    //     return await super.getElement(continueBttn)
-    // }
     async clickContinueBttn() {
         return await super.clickElement(continueBttn)
     }
     async isAccountDeletedVisible() {
-        const isVisible = await this.page.isVisible(this.accDeletedMssg);
+        const isVisible = await this.page.isVisible(accDeletedMssg);
         return isVisible;
     }
 }
